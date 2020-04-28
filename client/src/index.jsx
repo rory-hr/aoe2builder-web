9887e220b3b6cea2fb63ccfdede7e25ed9b22280
rory-roccio <rory.roccio@gmail.com>
2020-04-27 17:40:07 -0700

    Create and provide redux store to app

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { createStore } from 'react-redux';
+import { Provider } from 'react-redux';
+import allReducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />,
+  </Provider>,
+  document.getElementById('app')
+);
